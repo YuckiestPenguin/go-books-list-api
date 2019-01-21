0976e1e1088d854755742c296de2652d89707bf2
Vico Melo <vinicomelo@gmail.com>
2019-01-20 20:56:48 -0500

    Refactoring for sql prep

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 type Book struct {
@@ -19,18 +17,6 @@ var books []Book
 
 func main() {
 
-	books = append(books, Book{
-		ID:     1,
-		Title:  "The Life of Vico",
-		Author: "Vico",
-		Year:   "2019",
-	},
-		Book{
-			ID:     2,
-			Title:  "The Life of Roger",
-			Author: "Roger",
-			Year:   "2019",
-		})
 	router := mux.NewRouter()
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
@@ -43,64 +29,21 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	//just returns the books list as json for client
-	json.NewEncoder(w).Encode(books)
+
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-	//grabs the id param from the route
-	params := mux.Vars(r)
 
-	//converts the param books/1 from a string to int and assigns to "i"
-	i, _ := strconv.Atoi(params["id"])
-
-	//loops through all of the books and checks to see if the route param id matches any ids in the books slice
-	for _, book := range books {
-		if book.ID == i {
-			json.NewEncoder(w).Encode(&book)
-		}
-	}
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
-	//init a variable for new book to live it before sending
-	var book Book
-
-	//Grab the new book you are creating, grab the json version and turn it go code
-	json.NewDecoder(r.Body).Decode(&book)
-
-	//add that new book to books slice
-	books = append(books, book)
-
-	//send back the full books list slice as json for client to see
-	json.NewEncoder(w).Encode(books)
 
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
 
-	for i, item := range books {
-		if item.ID == book.ID {
-			books[i] = book
-		}
-	}
-
-	json.NewEncoder(w).Encode(books)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
-	//grabs the id param from the route
-	params := mux.Vars(r)
 
-	id, _ := strconv.Atoi(params["id"])
-
-	for i, book := range books {
-		if book.ID == id {
-			books = append(books[:i], books[i+1:]...)
-		}
-	}
-
-	json.NewEncoder(w).Encode(books)
 }
